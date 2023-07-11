65a0f59e8ba8db30ead3e1842b075efa90819d34
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2023-07-10 23:24:50 -0300

    get all e insercao de envelopes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 var axios = require('axios')
 const URL = "https://plataforma.astenassinatura.com.br/api/"
-var params = {
-    "token": "56ht9p-Li8k5zHaQ2Dzxzijr...",
-    "params": {}
-}
+const token = "56ht9p-Li8k5zHaQ2Dzxzijr..."
+
 var mysql      = require('mysql')
 var connection = mysql.createConnection({
     host     : 'localhost',
@@ -17,29 +16,34 @@ var connection = mysql.createConnection({
 
 // get all envelopes
 app.get('/', (req, res) => {
-    res.send('Hello World!')
     // salva todos dados dos envelopes
-    axios.get(URL + "/")
     connection.connect();
-
-    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-        if (err) throw err;
-        console.log('The solution is: ', rows[0].solution);
-    });
-
+    axios.post(URL + "/getRepositoriosDoUsuario", {"token": token, "params": { "idProprietario": 1}}).then((response) => {
+        var tmp = response.forEach(elem => elem.id);
+        tmp.forEach((id) => {
+            axios.post(URL + "/getEnvelopesByRepositorioOuPasta", {"token": token, "params": { "idRepositorio": id}}).then((list) => {
+                list.forEach(envelope => {
+                    connection.query('INSERT INTO `envelope` (`id`, `descricao`, `dataExpiracao`, `listaSignatariosEnvelope`) VALUES (envelope.id, envelope.descricao, envelope.dataHoraCriacao, envelope.usuario.id)', function(err, rows, fields) {
+                        if (err) throw err;
+                        console.log('Linhas modificadas ', rows);
+                    });
+                })
+                res.send(list)
+            })
+        })
+    })
     connection.end();
 })
 
 // criacao de envelopes
-app.get('/create-envelop/{id}', (req, res) => {
-    connection.connect();
-
-    connection.query("SELECT * FROM ")
-    connection.end();
-
+app.post('/create-envelop', (req, res) => {
+    var obj = req.body;
+    var params = {"token": token,"params":{"Envelope":{"descricao":obj.descricao,"Repositorio":{"id":obj.repositorio.id},"mensagem": obj.mensagem,"mensagemObservadores": obj.mensagemObservadores,"mensagemNotificacaoSMS": obj.mensagemNotificacaoSMS,"dataExpiracao": obj.dataExpiracao,"horaExpiracao": obj.horaExpiracao,"usarOrdem": obj.usarOrdem,"ConfigAuxiliar":{"documentosComXMLs": obj.ConfigAuxiliar.documentosComXMLs,"urlCarimboTempo": obj.ConfigAuxiliar.urlCarimboTempo},"listaDocumentos":{"Documento":[{"nomeArquivo":obj.listaDocumentos.Documento.nome,"mimeType":"application/pdf","conteudo":obj.listaDocumentos.Documento.conteudo}]}}}}
+    axios.post(URL+"/inserirEnvelope", params).then(response => res.send(response.mensagem));
 })
+
 // configuracao de signatarios
-app.get('/set-peploe/{id}',(req, res) => {
+app.get('/set-peploe/:id',(req, res) => {
     connection.connect();
 
     connection.query("SELECT * FROM ")
